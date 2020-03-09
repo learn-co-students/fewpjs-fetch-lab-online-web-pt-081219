0a822af639e1b21411f760e7bfde7887d48668a7
Shaqqour <mshaqqourm@gmail.com>
2020-03-09 12:43:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books").then(resp => resp.json()).then(json => console.log(json));
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main');
 
-  json.response.forEach(book => {
+  json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
@@ -14,5 +16,5 @@ function renderBooks(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  renderBooks(fetchBooks());
+  fetchBooks();
 })
