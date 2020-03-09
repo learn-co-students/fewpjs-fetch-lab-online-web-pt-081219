48554ea2b415d10407743a6360ed2895d9d879ac
Shaqqour <mshaqqourm@gmail.com>
2020-03-09 12:39:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books").then(resp => resp.json()).then(json => console.log(json));
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
+  const main = document.querySelector('main');
+
+  json.response.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
-  })
+  });
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  renderBooks(fetchBooks());
 })
