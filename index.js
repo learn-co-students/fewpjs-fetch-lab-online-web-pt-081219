027bca91ba534801b31bb1084887732218fa986e
kierra <51520037+kierrakay@users.noreply.github.com>
2020-03-20 17:43:17 -0400

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
+function fetchBooks() {
+
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+}	
+
+
+function renderBooks(json) {}
